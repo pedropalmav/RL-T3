1c795e9009c4e6f5317b5e0215ec654db84d2fe8
Pedro Palma <pedro.palma@uc.cl>
2024-10-26 23:29:24 -0300

    fix: Error when comparing to goal

--- agents/multi_goal/multi_goal_q_learning.py
+++ agents/multi_goal/multi_goal_q_learning.py
@@ -5,7 +5,7 @@ class MultiGoalQLearning(QLearning):
         for goal in goals:
             multi_goal_state = (state[0], goal)
             next_multi_goal_state = (next_state[0], goal)
-            if state[0] == goal:
+            if next_state[0] == goal:
                 td_error = 1 - self.get_q_value(multi_goal_state, action)
             else:
                 td_error = self.gamma * self.get_q_value(next_multi_goal_state, self.argmax(next_multi_goal_state)) - self.get_q_value(multi_goal_state, action)
